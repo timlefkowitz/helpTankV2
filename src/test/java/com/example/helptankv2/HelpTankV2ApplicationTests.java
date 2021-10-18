7a77f2b357c38d6e372fca79882e27c0a7b8fdbd
Timothy Lefkowitz <timothy.lefkowitz@gmail.com>
2021-10-18 11:58:14 -0500

    feat: helptankV2

---
+++ src/test/java/com/example/helptankv2/HelpTankV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helptankv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpTankV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
